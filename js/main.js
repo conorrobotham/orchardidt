a9b679616ff92c5735f116fae4fd30138399d191
conorr <conorrobotham@gmail.com>
2017-07-05 07:54:02 +1000

    Added javascript navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log('main');
+function addListeners(){
+    // your code
+    console.log('main');
+
+        document.getElementById("redSpoon").onclick = function () {
+            location.href = "html/red.html";
+            console.log('redSpoon clicked');
+        };
+        document.getElementById("greenSpoon").onclick = function () {
+            location.href = "html/green.html";
+            console.log('greenSpoon clicked');
+
+        };
+        document.getElementById("whiteSpoon").onclick = function () {
+            location.href = "html/white.html";
+            console.log('whiteSpoon clicked');
+        };
+
+
+}
+function coloursNav(){
+        document.getElementById("home").onclick = function () {
+            location.href = "../index.html";
+            console.log('home clicked');
+        };
+        document.getElementById("redPage").onclick = function () {
+            location.href = "red.html";
+            console.log('redPage clicked');
+        };
+        document.getElementById("greenPage").onclick = function () {
+            location.href = "green.html";
+            console.log('greenPage clicked');
+        };
+        document.getElementById("whitePage").onclick = function () {
+            location.href = "white.html";
+            console.log('whitePage clicked');
+        };
+}
